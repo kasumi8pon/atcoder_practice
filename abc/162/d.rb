97bbe03c31a8b174cdf98f26d0450d6634beb80a
kasumi <all.splashing@gmail.com>
2020-09-12 17:38:09 +0900

    RGB Triplets

--- abc/162/d.rb
+++ abc/162/d.rb
@@ -1,7 +1,24 @@
 n = gets.to_i
-chars = gets.chars
+s = gets.chomp.chars
 
-chars_with_index =
-  chars.each_with_object(Hash.new{ |hash, key| hash[key] = [] }).with_index(1) do |(char, result), i|
-    result[char] << i
+tally = s.tally
+tally.default = 0
+answer = tally['R'] * tally['G'] * tally['B']
+
+(0...n).each do |i|
+  (1..n).each do |d|
+    j = i + d
+    k = i + 2 * d
+    break if k >= n
+
+    if s[i] != s[j] && s[j] != s[k] && s[k] != s[i]
+      answer -= 1
+    end
   end
+end
+
+puts answer
+
+#  1 <= n <= 4000
+# 三重ループを二重ループにする問題
+# 1つめの条件を満たす値が RGB の掛け算の結果だと気づけないと、引き算にたどり着くことができない
