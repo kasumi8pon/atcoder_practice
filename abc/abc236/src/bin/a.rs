32e1f869ceba200f6911f666a2a4ef8756e1f8ec
kasumi teruya <all.splashing@gmail.com>
2023-08-11 22:29:29 +0900

    ABC236

---
+++ abc/abc236/src/bin/a.rs
@@ -0,0 +1,20 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        s: Chars,
+        a: usize,
+        b: usize
+    }
+
+    for i in 0..s.len() {
+        if i == (a - 1) {
+            print!("{}", s[b - 1]);
+        } else if i == (b - 1) {
+            print!("{}", s[a - 1]);
+        } else {
+            print!("{}", s[i]);
+        }
+    }
+}
