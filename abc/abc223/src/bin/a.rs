6b9db5c2738015bfe68792ff482f67fc67b345fe
kasumi teruya <all.splashing@gmail.com>
2023-08-22 20:17:14 +0900

    ABC223

---
+++ abc/abc223/src/bin/a.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        x: usize
+    }
+
+    if x % 100 == 0 && x >= 100 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
