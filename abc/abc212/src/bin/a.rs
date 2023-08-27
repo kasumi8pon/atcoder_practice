2301a0aa9c97c81f53a4ff16da326bd2319e1d28
kasumi teruya <all.splashing@gmail.com>
2023-08-27 21:57:57 +0900

    ABC212

---
+++ abc/abc212/src/bin/a.rs
@@ -0,0 +1,16 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: usize,
+        b: usize
+    }
+
+    if a > 0 && b == 0 {
+        println!("Gold");
+    } else if a == 0 && b > 0 {
+        println!("Silver");
+    } else {
+        println!("Alloy");
+    }
+}
