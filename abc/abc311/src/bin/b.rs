a7ec025a24b72092c7a0eef7a8ad83171d1f5ca8
kasumi teruya <all.splashing@gmail.com>
2023-08-28 21:26:28 +0900

    Vacation Together

--- abc/abc311/src/bin/b.rs
+++ abc/abc311/src/bin/b.rs
@@ -1,6 +1,29 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        n: usize,
+        d: usize,
+        s: [Chars; n]
     }
+
+    let mut answers = vec![];
+
+    let mut answer = 0;
+
+    for day in 0..d {
+        let all_free = s.iter().all(|schedule| schedule[day] == 'o');
+
+        if all_free {
+            answer += 1;
+        } else {
+            answers.push(answer);
+            answer = 0;
+        }
+    }
+
+    answers.push(answer);
+
+    println!("{}", answers.iter().max().unwrap());
 }
