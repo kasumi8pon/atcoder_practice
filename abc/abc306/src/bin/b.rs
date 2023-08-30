49a23d419b090dbb10c7f8c0b6abfd3294e19a21
kasumi teruya <all.splashing@gmail.com>
2023-08-30 20:27:01 +0900

    Base 2

--- abc/abc306/src/bin/b.rs
+++ abc/abc306/src/bin/b.rs
@@ -2,5 +2,11 @@ use proconio::input;
 
 fn main() {
     input! {
+        mut numbers: [usize; 64]
     }
+
+    numbers.reverse();
+    let joined_number: String = numbers.iter().map(|n| n.to_string()).collect();
+
+    println!("{}", usize::from_str_radix(&joined_number, 2).unwrap());
 }
