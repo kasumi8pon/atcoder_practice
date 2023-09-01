43cf644d5de915519cb2d2104cbc37c32c0b391c
kasumi teruya <all.splashing@gmail.com>
2023-09-01 21:55:44 +0900

    ABCDEFG

--- abc/abc305/src/bin/b.rs
+++ abc/abc305/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
+        p: char,
+        q: char
     }
+
+    let distance = [0, 3, 4, 8, 9, 14, 23];
+    let points =  ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+    let p_index = points.iter().position(|&x| x == p).unwrap();
+    let q_index = points.iter().position(|&x| x == q).unwrap();
+
+    println!("{}", ((distance[q_index] - distance[p_index]) as isize).abs());
 }
