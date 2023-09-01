54a9c37550f92982bc82b66f6d02573d68023544
kasumi teruya <all.splashing@gmail.com>
2023-09-01 22:24:53 +0900

    Subscribers

--- abc/abc304/src/bin/b.rs
+++ abc/abc304/src/bin/b.rs
@@ -2,5 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: u64
     }
+
+    let mut digit = 0;
+    let mut number = n.clone();
+
+    if number == 0 {
+        digit = 1;
+    } else {
+        while number != 0 {
+            digit += 1;
+            number /= 10;
+        }
+    }
+
+    let answer;
+
+    if digit > 3 {
+        answer = n - n % (10_u64.pow(digit - 3));
+    } else {
+        answer = n;
+    }
+
+    println!("{}", answer);
 }
