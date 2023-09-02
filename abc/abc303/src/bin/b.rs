f51f1157944a3e8bc9c1ac6a3715668559ef1d61
kasumi teruya <all.splashing@gmail.com>
2023-09-02 14:43:24 +0900

    Discord

--- abc/abc303/src/bin/b.rs
+++ abc/abc303/src/bin/b.rs
@@ -2,5 +2,35 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        a: [[usize; n]; m],
     }
+
+    let mut counted = vec![vec![false; n]; n];
+
+    for i in 0..m {
+        for j in 0..(n-1) {
+            let x;
+            let y;
+
+            if a[i][j] < a[i][j+1] {
+                x = a[i][j];
+                y = a[i][j+1];
+            } else {
+                x = a[i][j+1];
+                y = a[i][j];
+            }
+
+            counted[x - 1][y - 1] = true;
+        }
+    }
+
+    let mut count = 0;
+
+    for i in 0..n {
+        count += counted[i].iter().filter(|&&x| x == true).count();
+    }
+
+    println!("{}", ((n * (n - 1)) / 2) - count);
 }
