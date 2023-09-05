cd7a9078f2305a222fc1ed4cb9111d917f677caa
kasumi teruya <all.splashing@gmail.com>
2023-09-05 09:30:41 +0900

    Trick Tracking

--- abc/abc299/src/bin/b.rs
+++ abc/abc299/src/bin/b.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64,
+        t: u64,
+        c: [u64; n],
+        r: [u64; n]
+    }
+
+    let cr: Vec<_> = c.iter().zip(r.iter()).collect();
+
+    let same_colors: Vec<_> = cr.iter().filter(|&&(xc, _xr)| *xc == t).collect();
+
+    let number;
+    let answer;
+
+    if same_colors.len() >= 1 {
+        number = same_colors.iter().map(|&x| x.1).max().unwrap();
+        answer = cr.iter().position(|&(xc, xr)| *xc == t && *xr == *number ).unwrap();
+    } else {
+        let target_color = cr[0].0;
+        number = cr.iter().filter(|&&x| x.0 == target_color).map(|&x| x.1).max().unwrap();
+        answer = cr.iter().position(|(xc, xr)| *xc == target_color && *xr == number ).unwrap();
+    }
+
+    println!("{}", answer + 1);
 }
