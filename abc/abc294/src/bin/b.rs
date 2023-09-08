09e760097e5e64af386cefa0ef43213f40c11ad3
kasumi teruya <all.splashing@gmail.com>
2023-09-08 23:47:39 +0900

    ASCII Art

--- abc/abc294/src/bin/b.rs
+++ abc/abc294/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h],
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            if a[i][j] == 0 {
+                print!(".");
+            } else {
+                print!("{}", (b'A' + (a[i][j] - 1) as u8) as char);
+            }
+        }
+        println!("");
+    }
 }
