1281605d60a086021fd45c2385d7e0d727a536e5
kasumi teruya <all.splashing@gmail.com>
2023-09-08 23:38:55 +0900

    Chessboard

--- abc/abc296/src/bin/b.rs
+++ abc/abc296/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: [Chars; 8]
+    }
+
+    let vertical = ["8", "7", "6", "5", "4", "3", "2", "1"];
+    let horizontal = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+    for i in 0..8 {
+        for j in 0..8 {
+            if s[i][j] == '*' {
+                println!("{}{}", horizontal[j], vertical[i]);
+            }
+        }
+    }
 }
