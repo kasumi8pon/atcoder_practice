8fc619492a5205993806fe9f445330b2d845a0f1
kasumi teruya <all.splashing@gmail.com>
2023-09-09 17:55:46 +0900

    Call the ID Numb

--- abc/abc293/src/bin/b.rs
+++ abc/abc293/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let mut called = vec![false; n];
+
+    for i in 0..n {
+        if called[i] == false {
+            called[a[i] - 1] = true;
+        }
+    }
+
+    let count = called.iter().filter(|&&x| x == false).count();
+
+    println!("{}", count);
+    for i in 0..n {
+        if called[i] == false {
+            print!("{} ", i + 1);
+        }
+    }
 }
