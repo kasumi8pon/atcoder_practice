2cf12bdb3f879213ad61f09f044d4bde2434be78
kasumi teruya <all.splashing@gmail.com>
2023-09-11 09:21:18 +0900

    Yellow and Red

--- abc/abc292/src/bin/b.rs
+++ abc/abc292/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        q: usize,
+        events: [(usize, usize); q]
+    }
+
+    let mut cards = vec![0; n];
+
+    for event in events {
+        if event.0 == 1 {
+            cards[event.1 - 1] += 1;
+        } else if event.0 == 2 {
+            cards[event.1 - 1] = 2;
+        } else {
+            if cards[event.1 - 1] == 2 {
+                println!("Yes");
+            } else {
+                println!("No");
+            }
+        }
+    }
 }
