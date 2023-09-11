b7ead5ce61086d975d0583798e1add82042d4bfb
kasumi teruya <all.splashing@gmail.com>
2023-09-11 21:22:59 +0900

    Trimmed Mean

--- abc/abc291/src/bin/b.rs
+++ abc/abc291/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut x: [usize; 5 * n]
+    }
+
+    x.sort();
+
+    let available_scores = &x[n..5 * n - n];
+    let total_score: usize = available_scores.iter().sum();
+    let answer = total_score as f64 /(3.0 * n as f64);
+
+    println!("{}", answer);
 }
