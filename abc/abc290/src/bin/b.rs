04ccf21a40d94dc935e785700c10ed5afba0175c
kasumi teruya <all.splashing@gmail.com>
2023-09-12 20:52:11 +0900

    Qual B

--- abc/abc290/src/bin/b.rs
+++ abc/abc290/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        s: Chars
+    }
+
+    let mut count = 0;
+
+    for i in 0..n {
+        if s[i] == 'o' && count < k {
+            count += 1;
+            print!("o");
+        } else {
+            print!("x");
+        }
+    }
 }
