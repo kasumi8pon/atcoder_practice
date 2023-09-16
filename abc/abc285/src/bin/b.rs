db20750719b5e40a3b19687f810fc314b85935e5
kasumi teruya <all.splashing@gmail.com>
2023-09-16 15:02:25 +0900

    Longest Uncommon Prefix

--- abc/abc285/src/bin/b.rs
+++ abc/abc285/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: Chars
+    }
+
+    for i in 1..n {
+        let mut answer = n - i;
+        for l in 0..n - i {
+            if s[l] == s[l + i] {
+                answer = l;
+                break;
+            }
+        }
+        println!("{}", answer);
+    }
 }
