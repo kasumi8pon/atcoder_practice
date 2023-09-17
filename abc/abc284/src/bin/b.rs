93ccbcbd5cbac825589fadffe2c9217dafe8cbbb
kasumi teruya <all.splashing@gmail.com>
2023-09-17 20:50:45 +0900

    Multi Test Cases

--- abc/abc284/src/bin/b.rs
+++ abc/abc284/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
+        t: usize
+    }
+
+    for _ in 0..t {
+        input! {
+            n: usize,
+            a: [usize; n]
+        }
+
+        let answer = a.iter().filter(|&x| x % 2 != 0).count();
+
+        println!("{}", answer);
     }
 }
