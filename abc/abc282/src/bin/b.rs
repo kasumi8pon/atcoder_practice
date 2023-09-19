ef92dec4d2eb530ff7857a096fcdfcbf4c2d2a63
kasumi teruya <all.splashing@gmail.com>
2023-09-19 20:09:58 +0900

    Let's Get a Perf

--- abc/abc282/src/bin/b.rs
+++ abc/abc282/src/bin/b.rs
@@ -1,6 +1,30 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        s: [Chars; n],
     }
+
+    let mut answer = 0;
+
+    for i in 0..n {
+        for j in 0..n {
+            if i > j {
+                let mut count = true;
+                for k in 0..m {
+                    if s[i][k] == 'x' && s[j][k] == 'x' {
+                        count = false;
+                    }
+                }
+                if count {
+                    answer += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", answer);
 }
