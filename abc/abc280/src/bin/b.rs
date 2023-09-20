3068ceed35f4ccdbd9a87ab707f0635c3b921fa3
kasumi teruya <all.splashing@gmail.com>
2023-09-20 19:48:43 +0900

    Inverse Prefix S

--- abc/abc280/src/bin/b.rs
+++ abc/abc280/src/bin/b.rs
@@ -1,6 +1,20 @@
 use proconio::input;
+use itertools::Itertools;
 
 fn main() {
     input! {
+        n: usize,
+        s: [isize; n]
     }
+
+    let mut counter = 0;
+    let mut answers = vec![0; n];
+
+    for i in 0..n {
+        let a = s[i] - counter;
+        answers[i] = a;
+        counter += a;
+    }
+
+    println!("{}", answers.iter().join(" "));
 }
