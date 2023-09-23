1e83379b781c55db26549a087d51ed94e1bfaa73
kasumi teruya <all.splashing@gmail.com>
2023-09-23 18:32:09 +0900

    Line Sensor

--- abc/abc274/src/bin/b.rs
+++ abc/abc274/src/bin/b.rs
@@ -1,6 +1,22 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        h: usize,
+        w: usize,
+        c: [Chars; h]
+    }
+
+    for i in 0..w {
+        let mut count = 0;
+
+        for j in 0..h {
+            if c[j][i] == '#' {
+                count += 1;
+            }
+        }
+
+        println!("{}", count);
     }
 }
