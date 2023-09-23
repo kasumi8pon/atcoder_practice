f601a902c7fe5c1776035eb044cf7f8e19fbe870
kasumi teruya <all.splashing@gmail.com>
2023-09-23 18:23:47 +0900

    Playing Cards Validation

--- abc/abc277/src/bin/b.rs
+++ abc/abc277/src/bin/b.rs
@@ -1,6 +1,31 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        n: usize,
+        mut s: [Chars; n]
     }
+
+    let mut answer = "Yes";
+
+    for i in 0..n {
+        if ['H', 'D', 'C', 'S'].iter().any(|&x| x == s[i][0]) &&
+            ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'].iter().any(|&x| x == s[i][1]) {
+            continue;
+            } else {
+                answer = "No";
+                break;
+            }
+    }
+
+    let size = s.len();
+    s.sort();
+    s.dedup();
+
+    if s.len() != size {
+        answer = "No";
+    }
+
+    println!("{}", answer);
 }
