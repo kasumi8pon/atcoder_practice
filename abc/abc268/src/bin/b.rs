6bc8a8a414d167ac68a9b44c7671d274fbd12d66
kasumi teruya <all.splashing@gmail.com>
2023-09-26 21:24:35 +0900

    Prefix?

--- abc/abc268/src/bin/b.rs
+++ abc/abc268/src/bin/b.rs
@@ -2,5 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
+        s: String,
+        t: String
+    }
+
+    if t.starts_with(&s) {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
