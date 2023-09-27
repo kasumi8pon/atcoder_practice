607b6a0ba8bba40427f7a3a42d039db8e27143c7
kasumi teruya <all.splashing@gmail.com>
2023-09-27 21:51:48 +0900

    Tournament Result

--- abc/abc261/src/bin/b.rs
+++ abc/abc261/src/bin/b.rs
@@ -1,6 +1,39 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        n: usize,
+        a: [Chars; n]
     }
+
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                if a[i][j] != '-' {
+                    println!("incorrect");
+                    return;
+                }
+            } else if i < j {
+                if a[i][j] == 'W' {
+                    if a[j][i] != 'L' {
+                        println!("incorrect");
+                        return;
+                    }
+                } else if a[i][j] == 'L' {
+                    if a[j][i] != 'W' {
+                        println!("incorrect");
+                        return;
+                    }
+                } else {
+                    if a[j][i] != 'D' {
+                        println!("incorrect");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("correct");
 }
