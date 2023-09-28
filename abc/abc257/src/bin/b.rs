fc1f13a1436711dc7a75c8adf29e542e7a956bc4
kasumi teruya <all.splashing@gmail.com>
2023-09-28 23:37:26 +0900

    1D Pawn

--- abc/abc257/src/bin/b.rs
+++ abc/abc257/src/bin/b.rs
@@ -1,6 +1,26 @@
 use proconio::input;
+use itertools::Itertools;
 
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        q: usize,
+        mut a: [usize; k],
+        l: [usize; q]
     }
+
+    for num in l.iter() {
+        if a[num - 1] != n {
+            if num < &k {
+                if a[num - 1] + 1 != a[*num] {
+                    a[num - 1] += 1;
+                }
+            } else {
+                a[num - 1] += 1;
+            }
+        }
+    }
+
+    println!("{}", a.iter().join(" "));
 }
