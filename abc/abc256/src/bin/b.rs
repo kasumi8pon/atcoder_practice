241922a763fb14bbb4f418d58f47aa9beec79ab7
kasumi teruya <all.splashing@gmail.com>
2023-10-01 22:13:36 +0900

    Batters

--- abc/abc256/src/bin/b.rs
+++ abc/abc256/src/bin/b.rs
@@ -2,5 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n]
     }
+
+    let mut p = 0;
+    let mut bases = vec![false; 4];
+
+    for i in 0..n {
+        bases[0] = true;
+
+        for j in vec![3, 2, 1, 0] {
+            if bases[j] == true {
+                if a[i] + j >= 4 {
+                    p += 1;
+                } else {
+                    bases[a[i] + j] = true;
+                }
+                bases[j] = false;
+            }
+        }
+    }
+
+    println!("{}", p);
 }
