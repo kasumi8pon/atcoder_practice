829ab0c16e86ad747552a096b7bd8b33d28a1cb8
kasumi teruya <all.splashing@gmail.com>
2023-10-01 22:47:09 +0900

    Practical Computing

--- abc/abc254/src/bin/b.rs
+++ abc/abc254/src/bin/b.rs
@@ -1,6 +1,24 @@
 use proconio::input;
+use itertools::Itertools;
 
 fn main() {
     input! {
+        n: usize
+    }
+
+    let mut numbers = vec![vec![]; n];
+
+    for i in 0..n {
+        for j in 0..=i {
+            if j == 0 || j == i {
+                numbers[i].push(1);
+            } else {
+                let x = numbers[i - 1][j - 1];
+                let y = numbers[i - 1][j];
+                numbers[i].push(x + y);
+            }
+        }
+
+        println!("{}", numbers[i].iter().join(" "));
     }
 }
