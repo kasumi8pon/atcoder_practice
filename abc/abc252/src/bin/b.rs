840641e44cb26ce4161f8a097463e45e54fe76ec
kasumi teruya <all.splashing@gmail.com>
2023-10-03 19:35:02 +0900

    Takahashi's Fail

--- abc/abc252/src/bin/b.rs
+++ abc/abc252/src/bin/b.rs
@@ -2,5 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+        b: [usize; k]
     }
+
+    let max = a.iter().max().unwrap();
+
+    for i in 0..n {
+        if a[i] == *max {
+            let find = b.iter().find(|&&x| x == i + 1);
+            if find.is_some() {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
 }
