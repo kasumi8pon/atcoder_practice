2f111dee701231abec0c29bb54f4be3a8176499e
kasumi teruya <all.splashing@gmail.com>
2023-10-04 19:42:03 +0900

    Perfect String

--- abc/abc249/src/bin/b.rs
+++ abc/abc249/src/bin/b.rs
@@ -1,6 +1,24 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        mut s: Chars
+    }
+
+    let all_uppercase = s.iter().all(|&c| c.is_uppercase());
+    let all_lowercase = s.iter().all(|&c| c.is_lowercase());
+
+    let origin_size = s.len();
+    s.sort();
+    s.dedup();
+    let dedup_size = s.len();
+
+    let unique = origin_size == dedup_size;
+
+    if !all_uppercase && !all_lowercase && unique {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
