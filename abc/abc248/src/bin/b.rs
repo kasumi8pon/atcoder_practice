0bb730513a96bad6e05439ce8ec9a72449252b3a
kasumi teruya <all.splashing@gmail.com>
2023-10-04 19:48:16 +0900

    Slimes

--- abc/abc248/src/bin/b.rs
+++ abc/abc248/src/bin/b.rs
@@ -2,5 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
+        mut a: usize,
+        b: usize,
+        k: usize
     }
+
+    let mut answer = 0;
+
+    loop {
+        if a >= b {
+            break;
+        }
+
+        a *= k;
+        answer += 1;
+    }
+
+    println!("{}", answer);
 }
