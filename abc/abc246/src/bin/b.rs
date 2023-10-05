0293cb259d03c069f9de1df1a0fb98248e46e943
kasumi teruya <all.splashing@gmail.com>
2023-10-05 19:19:47 +0900

    Get Closer

--- abc/abc246/src/bin/b.rs
+++ abc/abc246/src/bin/b.rs
@@ -2,5 +2,11 @@ use proconio::input;
 
 fn main() {
     input! {
+        a: usize,
+        b: usize
     }
+
+    let d = (a.pow(2) as f32 + b.pow(2) as f32).sqrt();
+
+    println!("{} {}", a as f32 / d, b as f32 / d);
 }
