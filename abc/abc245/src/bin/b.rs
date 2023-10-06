cd89c3a61adb73a227863e8bce86b64e51c19bbc
kasumi teruya <all.splashing@gmail.com>
2023-10-06 21:50:54 +0900

    Mex

--- abc/abc245/src/bin/b.rs
+++ abc/abc245/src/bin/b.rs
@@ -2,5 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        mut a: [usize; n],
+    }
+
+    a.sort();
+    a.dedup();
+
+    let mut counter = 0;
+
+    for i in 0..=2000 {
+        if counter == a.len() {
+            println!("{}", i);
+            return;
+        }
+
+        if i < a[counter] {
+            println!("{}", i);
+            return;
+        } else {
+            counter += 1;
+        }
     }
 }
