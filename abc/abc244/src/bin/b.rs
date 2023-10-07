17cf42ba242451acf80fb5eb3d84ec6841fc499f
kasumi teruya <all.splashing@gmail.com>
2023-10-07 20:56:25 +0900

    Go Straight and Turn Right

--- abc/abc244/src/bin/b.rs
+++ abc/abc244/src/bin/b.rs
@@ -1,6 +1,31 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        n: usize,
+        t: Chars
     }
+
+    let mut position = (0, 0);
+    let mut direction = (1, 0);
+
+    for i in 0..n {
+        if t[i] == 'S' {
+            position.0 += direction.0;
+            position.1 += direction.1;
+        } else {
+            if direction == (1, 0) {
+                direction = (0, -1);
+            } else if direction == (0, -1) {
+                direction = (-1, 0);
+            } else if direction == (-1, 0) {
+                direction = (0, 1);
+            } else {
+                direction = (1, 0);
+            }
+        }
+    }
+
+    println!("{} {}", position.0, position.1);
 }
