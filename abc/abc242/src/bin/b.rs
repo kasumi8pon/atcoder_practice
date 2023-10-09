7d471c2f2abefe81fcd6f8af39fdba1384cb5a68
kasumi teruya <all.splashing@gmail.com>
2023-10-09 20:35:52 +0900

    Minimize Ordering

--- abc/abc242/src/bin/b.rs
+++ abc/abc242/src/bin/b.rs
@@ -1,6 +1,13 @@
 use proconio::input;
+use proconio::marker::Chars;
+use itertools::Itertools;
 
 fn main() {
     input! {
+        mut s: Chars
     }
+
+    s.sort();
+
+    println!("{}", s.iter().join(""));
 }
