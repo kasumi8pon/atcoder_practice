99cc34d10e1b91596a38ddbffeba75f54276a715
kasumi teruya <all.splashing@gmail.com>
2023-10-09 20:29:54 +0900

    Hit and Blow

--- abc/abc243/src/bin/b.rs
+++ abc/abc243/src/bin/b.rs
@@ -2,5 +2,25 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n],
+        b: [usize; n]
     }
+
+    let mut answer1 = 0;
+    let mut answer2 = 0;
+
+    for i in 0..n {
+        if a[i] == b[i] {
+            answer1 += 1;
+        }
+        for j in 0..n {
+            if a[i] == b[j] && i != j {
+                answer2 += 1;
+            }
+        }
+    }
+
+    println!("{}", answer1);
+    println!("{}", answer2);
 }
