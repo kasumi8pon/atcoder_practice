4c74d662ec620c586f019d2901401fca7971b20a
kasumi teruya <all.splashing@gmail.com>
2023-10-10 19:20:00 +0900

    Pasta

--- abc/abc241/src/bin/b.rs
+++ abc/abc241/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+        b: [usize; m]
     }
+
+    for size in b.iter() {
+        let position = a.iter().position(|&x| x == *size);
+        match position {
+            Some(n) => { a[n] = 0; },
+            None => { println!("No"); return; }
+        }
+    }
+
+    println!("Yes");
 }
