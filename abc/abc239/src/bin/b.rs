ee513e4fea7fb6ee16ed67b3a3d07c1e50ccf726
kasumi teruya <all.splashing@gmail.com>
2023-10-11 20:41:31 +0900

    Integer Division

--- abc/abc239/src/bin/b.rs
+++ abc/abc239/src/bin/b.rs
@@ -2,5 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
+        x: i64
     }
+
+    let answer;
+
+    if x >= 0 {
+        answer = x / 10;
+    } else {
+        answer = (x - 9) / 10;
+    }
+
+    println!("{}", answer);
 }
