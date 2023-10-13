3b14fa96fc82dc8536106ce8d4a84eec79f84a27
kasumi teruya <all.splashing@gmail.com>
2023-10-13 23:38:20 +0900

    Who is missing?

--- abc/abc236/src/bin/b.rs
+++ abc/abc236/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [usize; 4 * n - 1]
     }
+
+    let mut counter = vec![0; n];
+
+    for number in a {
+        counter[number - 1] += 1;
+    }
+
+    let answer = counter.iter().position(|&x| x == 3).unwrap() + 1;
+
+    println!("{}", answer);
 }
