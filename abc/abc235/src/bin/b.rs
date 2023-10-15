fcc3601badd7e58d5e3aec472f543ee73c7bef46
kasumi teruya <all.splashing@gmail.com>
2023-10-15 15:36:26 +0900

    Climbing Takahashi

--- abc/abc235/src/bin/b.rs
+++ abc/abc235/src/bin/b.rs
@@ -2,5 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        h: [usize; n]
     }
+
+    for i in 0..=(n - 2) {
+        if h[i] >= h[i + 1] {
+            println!("{}", h[i]);
+            return;
+        }
+    }
+    println!("{}", h[n - 1]);
 }
