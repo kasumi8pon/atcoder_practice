5cd919323062bd5701d86e864eaa54c249a02969
kasumi teruya <all.splashing@gmail.com>
2023-10-15 15:47:27 +0900

    Longest Segment

--- abc/abc234/src/bin/b.rs
+++ abc/abc234/src/bin/b.rs
@@ -2,5 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        xy: [(isize, isize); n]
     }
+
+    let mut distances = vec![];
+
+    for i in 0..n {
+        for j in 0..n {
+            if i != j {
+                let x1 = xy[i].0;
+                let y1 = xy[i].1;
+                let x2 = xy[j].0;
+                let y2 = xy[j].1;
+
+                let distance = (x1 - x2).pow(2) + (y1 - y2).pow(2);
+
+                distances.push(distance);
+            }
+        }
+    }
+
+    let max = distances.iter().max().unwrap();
+
+    println!("{}", (*max as f64).sqrt());
 }
