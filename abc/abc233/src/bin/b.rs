ee39b95d2582bf3ed5611ba3474db8bf9d785f1b
kasumi teruya <all.splashing@gmail.com>
2023-10-16 22:44:21 +0900

    Reverse

--- abc/abc233/src/bin/b.rs
+++ abc/abc233/src/bin/b.rs
@@ -1,6 +1,21 @@
 use proconio::input;
+use proconio::marker::Chars;
+use proconio::marker::Usize1;
 
 fn main() {
     input! {
+        l: Usize1,
+        r: Usize1,
+        s: Chars
+    }
+
+    let length = s.len();
+
+    for i in 0..length {
+        if l <= i && i <= r {
+            print!("{}", s[r + l - i]);
+        } else {
+            print!("{}", s[i]);
+        }
     }
 }
