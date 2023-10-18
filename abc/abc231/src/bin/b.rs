c5a29328494011f53303062413efca7e616aea48
kasumi teruya <all.splashing@gmail.com>
2023-10-18 22:37:06 +0900

    Election

--- abc/abc231/src/bin/b.rs
+++ abc/abc231/src/bin/b.rs
@@ -1,6 +1,18 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
+        n: usize,
+        s: [String; n]
     }
+
+    let mut votes = HashMap::new();
+    for name in s.iter() {
+        *votes.entry(name).or_insert(0) += 1;
+    }
+
+    let answer = votes.iter().max_by_key(|&(_, v)| v).unwrap().0;
+
+    println!("{}", answer);
 }
