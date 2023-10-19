7deae6af19bb07938ab6fb3fd8e609d043867a01
kasumi teruya <all.splashing@gmail.com>
2023-10-19 21:58:57 +0900

    Triple Metre

--- abc/abc230/src/bin/b.rs
+++ abc/abc230/src/bin/b.rs
@@ -1,6 +1,60 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        s: Chars
     }
+
+    let size = s.len();
+
+    if s[0] == 'o' {
+        for i in 0..size {
+            if i % 3 == 0 {
+                if s[i] != 'o' {
+                    println!("No");
+                    return;
+                }
+            } else {
+                if s[i] != 'x' {
+                    println!("No");
+                    return;
+                }
+            }
+        }
+    } else if s[0] == 'x' {
+        if size > 1 {
+            if s[1] == 'o' {
+                for i in 0..size {
+                    if i % 3 == 1 {
+                        if s[i] != 'o' {
+                            println!("No");
+                            return;
+                        }
+                    } else {
+                        if s[i] != 'x' {
+                            println!("No");
+                            return;
+                        }
+                    }
+                }
+            } else {
+                for i in 0..size {
+                    if i % 3 == 2 {
+                        if s[i] != 'o' {
+                            println!("No");
+                            return;
+                        }
+                    } else {
+                        if s[i] != 'x' {
+                            println!("No");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Yes");
 }
