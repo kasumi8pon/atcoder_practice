bc1abe2f131109bbd3973dd2338aa0c361a35f83
kasumi teruya <all.splashing@gmail.com>
2023-10-21 19:28:20 +0900

    Hard Calculation

--- abc/abc229/src/bin/b.rs
+++ abc/abc229/src/bin/b.rs
@@ -1,6 +1,23 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        a: Chars,
+        b: Chars
     }
+
+    let a_digit = a.len();
+    let b_digit = b.len();
+    let digits = vec![a_digit, b_digit];
+    let min_digit = *digits.iter().min().unwrap();
+
+    for i in 0..min_digit {
+        if (a[a_digit - 1 - i] as u32 - '0' as u32) + (b[b_digit - 1 - i] as u32 - '0' as u32) > 9 {
+            println!("Hard");
+            return;
+        }
+    }
+
+    println!("Easy");
 }
