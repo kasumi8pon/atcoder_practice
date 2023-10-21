8be31c01868d49a7852d3deb8edbd6e38d5b3d4c
kasumi teruya <all.splashing@gmail.com>
2023-10-21 19:28:34 +0900

    Takahashi's Secret

--- abc/abc228/src/bin/b.rs
+++ abc/abc228/src/bin/b.rs
@@ -2,5 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        x: usize,
+        a: [usize; n]
     }
+
+    let mut know = vec![false; n];
+
+    let mut target = x - 1;
+
+    loop {
+        if know[target] {
+            break;
+        } else {
+            know[target] = true;
+            target = a[target] - 1;
+        }
+    }
+
+    println!("{}", know.iter().filter(|&&x| x).count());
 }
