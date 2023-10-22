71817c1d3dde8acdc9d7d7ea67dbb82ae96344d9
kasumi teruya <all.splashing@gmail.com>
2023-10-22 19:19:51 +0900

    KEYENCE building

--- abc/abc227/src/bin/b.rs
+++ abc/abc227/src/bin/b.rs
@@ -2,5 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        s: [usize; n]
     }
+
+    let mut candidates = vec![];
+
+    for a in 1..=1000 {
+        for b in 1..=1000 {
+            candidates.push(4 * a * b + 3 * a + 3 * b);
+        }
+    }
+
+    candidates.sort();
+    candidates.dedup();
+
+    let mut answer = 0;
+
+    for t in s {
+        if !candidates.contains(&t) {
+            answer += 1;
+        }
+    }
+
+    println!("{}", answer);
 }
