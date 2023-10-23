8e4679768c70fa0dd42bfe3faf70b7bcf839672e
kasumi teruya <all.splashing@gmail.com>
2023-10-23 21:27:06 +0900

    Star or Not

--- abc/abc225/src/bin/b.rs
+++ abc/abc225/src/bin/b.rs
@@ -2,5 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        trees: [(usize, usize); n - 1]
     }
+
+    let mut lines = vec![0; n];
+
+    for tree in trees {
+        lines[tree.0 - 1] += 1;
+        lines[tree.1 - 1] += 1;
+    }
+
+    for line in lines {
+        if line == n - 1 {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
