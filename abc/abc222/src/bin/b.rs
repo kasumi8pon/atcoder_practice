feed66505cdd29258fe1d6ffa468d8df14fdfa1c
kasumi teruya <all.splashing@gmail.com>
2023-10-26 21:51:02 +0900

    Failing Grade

--- abc/abc222/src/bin/b.rs
+++ abc/abc222/src/bin/b.rs
@@ -2,5 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        p: usize,
+        a: [usize; n]
     }
+
+    let mut answer = 0;
+
+    for i in 0..n {
+        if a[i] < p {
+            answer += 1;
+        }
+    }
+
+    println!("{}", answer);
 }
