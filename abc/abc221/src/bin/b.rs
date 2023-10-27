d219e596320ec75d861e89c5f616bcc073fc3b34
kasumi teruya <all.splashing@gmail.com>
2023-10-27 23:27:02 +0900

    typo

--- abc/abc221/src/bin/b.rs
+++ abc/abc221/src/bin/b.rs
@@ -1,6 +1,34 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        s: Chars,
+        t: Chars
+    }
+
+    let size = s.len();
+    let mut unmatched = vec![];
+
+    for i in 0..size {
+        if s[i] != t[i] {
+            unmatched.push(i);
+        }
+    }
+
+    if unmatched.len() == 0 {
+        println!("Yes");
+    } else if unmatched.len() == 2 {
+        if unmatched[1] - unmatched[0] == 1 {
+            if s[unmatched[0]] == t[unmatched[1]] && s[unmatched[1]] == t[unmatched[0]] {
+                println!("Yes");
+            } else {
+                println!("No");
+            }
+        } else {
+            println!("No");
+        }
+    } else {
+        println!("No");
     }
 }
