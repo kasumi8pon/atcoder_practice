531f0e7b6b4720460eebbad6da87e8c215c5555d
kasumi teruya <all.splashing@gmail.com>
2023-10-29 22:23:45 +0900

    Maritozzo

--- abc/abc219/src/bin/b.rs
+++ abc/abc219/src/bin/b.rs
@@ -1,6 +1,18 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        s1: String,
+        s2: String,
+        s3: String,
+        t: Chars
+    }
+
+    let strings = vec![s1, s2, s3];
+
+    for i in t {
+        let index = i.to_digit(10).unwrap() as usize;
+        print!("{}", strings[index - 1])
     }
 }
