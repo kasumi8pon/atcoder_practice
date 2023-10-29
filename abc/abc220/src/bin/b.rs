b08646f9ba2f49121bd31551df7f81b24f5427d9
kasumi teruya <all.splashing@gmail.com>
2023-10-29 22:11:52 +0900

    Base K

--- abc/abc220/src/bin/b.rs
+++ abc/abc220/src/bin/b.rs
@@ -2,5 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
+        k: u32,
+        a: String,
+        b: String,
     }
+
+    let x = usize::from_str_radix(&a, k).unwrap();
+    let y = usize::from_str_radix(&b, k).unwrap();
+
+    println!("{}", x * y);
 }
