0b1a5bf779b8cae467c7fe53764d9893943ffae9
kasumi teruya <all.splashing@gmail.com>
2023-10-31 22:05:12 +0900

    AtCoder Quiz

--- abc/abc217/src/bin/b.rs
+++ abc/abc217/src/bin/b.rs
@@ -2,5 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
+        s1: String,
+        s2: String,
+        s3: String
+    }
+
+    let contest = vec!["ABC", "ARC", "AGC", "AHC"];
+
+    for i in contest {
+        if i != &s1 && i != &s2 && i != &s3 {
+            println!("{}", i);
+        }
     }
 }
