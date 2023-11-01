b620ba60ac1c12250da30d1dcfc342ecc72b969d
kasumi teruya <all.splashing@gmail.com>
2023-11-01 22:16:21 +0900

    Same Name

--- abc/abc216/src/bin/b.rs
+++ abc/abc216/src/bin/b.rs
@@ -2,5 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        names: [(String, String); n]
     }
+
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+
+            if names[i].0 == names[j].0 && names[i].1 == names[j].1 {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
 }
