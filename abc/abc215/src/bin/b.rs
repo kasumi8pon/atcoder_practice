eccb5d4a8fc0facdcb8183fe3ff4f13e8bdd7e8f
kasumi teruya <all.splashing@gmail.com>
2023-11-02 22:55:49 +0900

    log2(N)

--- abc/abc215/src/bin/b.rs
+++ abc/abc215/src/bin/b.rs
@@ -2,5 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: u64
+    }
+
+    for i in 0.. {
+        if 2u64.pow(i) > n {
+            println!("{}", i - 1);
+            return;
+        }
     }
 }
