2de62d305d37cbbd6efba3b53b3ea668fbac231e
kasumi teruya <all.splashing@gmail.com>
2023-11-03 21:27:11 +0900

    How many?

--- abc/abc214/src/bin/b.rs
+++ abc/abc214/src/bin/b.rs
@@ -2,5 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
+        s: usize,
+        t: usize
     }
+
+    let mut answer = 0;
+
+    for i in 0..=s {
+        for j in 0..=s {
+            for k in 0..=s {
+                if i + j + k <= s && i * j * k <= t {
+                    answer += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", answer);
 }
