7f4d1175121882ecc2a5c1d5d751d4f8b0f78ee4
kasumi teruya <all.splashing@gmail.com>
2023-11-04 16:44:22 +0900

    Booby Prize

--- abc/abc213/src/bin/b.rs
+++ abc/abc213/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let mut sorted_a = a.clone();
+    sorted_a.sort();
+    sorted_a.reverse();
+    let score = sorted_a[1];
+
+    for i in 0..n {
+        if a[i] == score {
+            println!("{}", i + 1);
+            return;
+        }
     }
 }
