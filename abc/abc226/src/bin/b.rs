c28c2a0986714f31a45054d039b7e5cdf95e7712
kasumi teruya <all.splashing@gmail.com>
2023-11-05 17:05:49 +0900

    Counting Arrays

--- abc/abc226/src/bin/b.rs
+++ abc/abc226/src/bin/b.rs
@@ -2,5 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize
     }
+
+    let mut a = vec![];
+
+    for _i in 0..n {
+        input! {
+            l: usize,
+            line: [usize; l]
+        }
+
+        a.push(line);
+    }
+
+    a.sort();
+    a.dedup();
+
+    println!("{}", a.len());
 }
