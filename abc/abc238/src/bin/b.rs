77894cea336dce0ddbccb85db32be58014894dce
kasumi teruya <all.splashing@gmail.com>
2023-11-06 22:06:03 +0900

    Pizza

--- abc/abc238/src/bin/b.rs
+++ abc/abc238/src/bin/b.rs
@@ -2,5 +2,35 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    if n == 1 {
+        if a[0] < 180 {
+            println!("{}", 360 - a[0]);
+        } else {
+            println!("{}", a[0]);
+        }
+    } else {
+        let mut cuts = vec![0];
+        let mut current_angle = 0;
+
+        for i in a {
+            let angle = (current_angle + i) % 360;
+            current_angle = angle;
+
+            cuts.push(angle);
+        }
+
+        cuts.sort();
+
+        let mut angles = vec![];
+
+        for i in 1..=n {
+            angles.push(cuts[i] - cuts[i - 1]);
+        }
+
+        println!("{}", angles.iter().max().unwrap());
     }
 }
