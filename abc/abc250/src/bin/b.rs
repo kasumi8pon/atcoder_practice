c885ebecd2511a30c1ba994a15d502bd21d62c4d
kasumi teruya <all.splashing@gmail.com>
2023-11-08 23:39:52 +0900

    Enlarged Checker Board

--- abc/abc250/src/bin/b.rs
+++ abc/abc250/src/bin/b.rs
@@ -2,5 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: usize,
+        b: usize
+    }
+
+    for i in 0..n * a {
+        for j in 0..n * b {
+            if (i / a) % 2 == 0 {
+                if (j / b) % 2 == 0 {
+                    print!(".");
+                } else {
+                    print!("#");
+                }
+            } else {
+                if (j / b) % 2 == 0 {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+        }
+        println!();
     }
 }
