dd239684b8b878fea5f008ca9e2295eb85f56cdd
kasumi teruya <all.splashing@gmail.com>
2023-11-09 22:23:21 +0900

    At Most 3 (Judge ver.)

--- abc/abc251/src/bin/b.rs
+++ abc/abc251/src/bin/b.rs
@@ -2,5 +2,43 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        w: usize,
+        mut a: [usize; n]
     }
+
+    let mut answers = vec![false; w];
+
+    for i in 0..n {
+        let sum = a[i];
+
+        if sum <= w {
+            answers[sum - 1] = true;
+        }
+
+        for j in 0..n {
+            if j == i {
+                continue;
+            }
+
+            let sum = sum + a[j];
+
+            if sum <= w {
+                answers[sum - 1] = true;
+            }
+
+            for k in 0..n {
+                if k == j || k == i {
+                    continue;
+                }
+                let sum = sum + a[k];
+
+                if sum <= w {
+                    answers[sum - 1] = true;
+                }
+            }
+        }
+    }
+
+    println!("{}", answers.iter().filter(|&&x| x).count());
 }
